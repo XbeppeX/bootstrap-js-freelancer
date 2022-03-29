f3146db1b15538f565be425cdd556feef4a0531c
giusepperusso412 <russogiuseppe412@gmail.com>
2022-03-29 16:02:27 +0200

    coupon2

--- main.js
+++ main.js
@@ -5,21 +5,26 @@ function myFunction() {
     let coupon = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
     let newCoupon = document.getElementById("discount").value;
     const sconto25 = 25 / 100;
-    
+    let costoBack = ore * 20.5;
+    let costoFront = ore * 15.3;
+    let costoAnalist = ore * 33.6;
 
 
     if ((list === '1') && (coupon.includes(newCoupon)))  {
-        let costoBack = ore * 20.5;
         const totale = costoBack - (costoBack * sconto25);
         document.getElementById("bottone").innerHTML = `${totale.toFixed(2)} €`;
     } else if ((list === '2') && (coupon.includes(newCoupon))) {
-        let costoFront = ore * 15.3;
         const totale2 = costoFront - (costoFront * sconto25);
         document.getElementById("bottone").innerHTML = `${totale2.toFixed(2)} €`;
     } else if ((list === '3') && (coupon.includes(newCoupon))) {
-        let costoAnalist = ore * 33.6;
         const totale3 = costoAnalist - (costoAnalist * sconto25);
         document.getElementById("bottone").innerHTML = `${totale3.toFixed(2)} €`;
+    } else if (list === '1') {
+        document.getElementById("bottone").innerHTML = `${costoBack.toFixed(2)} €`;
+    } else if (list === '2') {
+        document.getElementById("bottone").innerHTML = `${costoFront.toFixed(2)} €`;
+    } else if (list === '3') {
+        document.getElementById("bottone").innerHTML = `${costoAnalist.toFixed(2)} €`;
     }
     
 }
